4e01ee1a13134feee74d20faf33b4c2a00acfd4a
OneGulshan <gulshankumar.mailid01@gmail.com>
2023-07-02 19:28:44 +0530

    Changes

--- Dom/dom-main.js
+++ Dom/dom-main.js
@@ -49,4 +49,98 @@ element = document.querySelectorAll("#header h1");//Advance Selector
 element = document.querySelectorAll("#header h1")[0].innerHTML;
 console.log(element);
 
-//65
+element = document.querySelector("#menu").style;//65//Style
+element = document.querySelector("#menu").style.border;
+document.querySelector("#menu").style.backgroundColor = "green";
+document.querySelector("#menu").style.color = "blue";
+element = document.querySelector("#menu").style.color;
+document.querySelector("#sidebar").className = "yxz";//ClassName
+document.querySelector("#sidebar").className = "yxz zxy";//Last Class Priority
+element = document.querySelector("#sidebar").className;
+document.querySelector("#sidebar").classList = "yxz zxy";//ClssList
+document.querySelector("#sidebar").classList.add("asd","efg");
+document.querySelector("#sidebar").classList.remove("yxz", "zxy");
+element = document.querySelector("#sidebar").classList;
+element = document.querySelector("a[href='#navhome']").style.background = 'blue';
+console.log(element);
+
+document.getElementById("menu").onclick = abc;//66
+document.getElementById("menu").onmouseleave = abc;
+function abc(){
+    document.getElementById("menu").style.background = "pink";
+}
+document.getElementById("menu").addEventListener("click", abc);//Event(without on) with Function
+document.getElementById("menu").addEventListener("click", function(){//addEventListener with In Body Function & same event
+    // document.getElementById("menu").style.border = "10px solid gold";
+    this.style.border = "10px solid gold";//this means by same object
+});
+document.querySelector("#outer").addEventListener("click", function(){
+    alert('Outer Div');
+}, true);
+
+document.querySelector("#inner").addEventListener("click", function(){
+    alert('Inner Div');
+}, true);
+
+document.querySelector("#sidebar").addEventListener("mouseleave", aaa); 
+document.querySelector("#sidebar").addEventListener("click", bbb); 
+function aaa(){
+    document.querySelector("#sidebar").style.background = "silver";
+}
+function bbb(){//On Click mouseleave event removed
+    document.querySelector("#sidebar").removeEventListener('mouseleave', aaa);
+}
+
+document.getElementById("sidebar").addEventListener("click", abc);//67
+function abc(){
+    document.querySelector("a[href='#sidehome']").classList.add("pp", "cdf");
+    document.querySelector("a[href='#sidehome']").classList.remove("cdf");
+    var a = document.querySelector("a[href='#sidehome']").classList.length;
+    document.querySelector("a[href='#sidehome']").classList.toggle("dfg");
+    var b = document.getElementById("sidebar").classList.item(1);
+    var c = document.getElementById("sidebar").classList.contains("asd");
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+var a = document.getElementById("o").parentElement;//68
+var a = document.body.parentElement;
+document.getElementById("i").parentElement.style.background = "red";
+var a = document.getElementById("i").parentElement;
+document.getElementById("child-c").parentElement.style.background = "green";
+var a = document.getElementById("child-c").parentElement;
+document.getElementById("child-c").parentElement.style.background = "green";
+var a = document.getElementById("child-c").parentElement;
+document.getElementById("child-c").parentNode.style.background = "purple";
+var a = document.getElementById("child-c").parentNode;
+var a = document.getElementById("main").parentNode;
+var a = document.getElementById("main").parentElement;
+console.log(a);
+
+var a = document.getElementById("o").children;//69
+var a = document.getElementById("i").children;
+var a = document.getElementById("i").children[1];
+var a = document.getElementById("i").children[0].style.background = "blue";
+var a = document.getElementById("i").children[1].style.background = "green";
+var a = document.getElementById("i").children[0].innerHTML;
+var b = document.getElementById("i").children;
+var c = document.getElementById("i").childNodes;
+var a = document.getElementById("i").childNodes[0].innerHTML;//undefine(bec of Enter)
+var a = document.getElementById("i").childNodes[5].style.background = "pink";
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = document.getElementById("i").firstElementChild;//70
+var a = document.getElementById("i").firstElementChild.innerHTML;
+var a = document.getElementById("i").firstElementChild.style.background = "gray";
+var a = document.getElementById("i").lastElementChild.style.background = "yellow";
+var a = document.getElementById("o").firstElementChild.style.background = "silver";
+var a = document.getElementById("o").lastElementChild.style.background = "blue";
+var a = document.getElementById("i").firstChild;
+var a = document.getElementById("i").lastChild;
+// var a = document.getElementById("i").lastChild.style.background = "blue";
+var a = document.getElementById("child-c").fistChild;//Same value 'C' return because of no space(no any child) simple
+var a = document.getElementById("child-c").lastChild;
+console.log(a);
